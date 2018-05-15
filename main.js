8f4c0c115058d39ebaefcf12da6c54638c98940b
Marin Petrunić <marin.petrunic@gmail.com>
2018-05-15 21:59:11 +0200

    adds sentry

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import Expo, { AppLoading } from 'expo';
 import React from 'react';
 import { Root, StyleProvider } from 'native-base';
+import Sentry from '@mpetrunic/sentry-expo';
 import Router from './navigation/Router';
 import AuthService from './services/AuthService';
 import getTheme from './native-base-theme/components';
 
+// Remove this once Sentry is correctly setup.
+Sentry.enableInExpoDevelopment = true;
+
+Sentry.config('https://290322b7e0554cce9b783cad053e633e@sentry.io/1207215').install();
 
 class App extends React.Component {
   constructor(props) {
@@ -12,31 +17,36 @@ class App extends React.Component {
 
     this.auth = new AuthService();
     this.state = {
-      fontsAreLoaded: false,
+      isReady: false,
       initialRoute: null,
     };
   }
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
-    });
-
+  async initApp() {
     const isAuthenticated = await this.auth.isAuthenticated();
     if (isAuthenticated) {
       this.setState({ initialRoute: 'home' });
     } else {
       this.setState({ initialRoute: 'login' });
     }
+    this.setState({ isReady: true });
+  }
 
-    this.setState({ fontsAreLoaded: true });
+  async loadAssets() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
   }
 
   render() {
-    if (!this.state.fontsAreLoaded) {
-      return <AppLoading />;
+    if (!this.state.isReady) {
+      return (<AppLoading
+        startAsync={this.loadAssets}
+        onFinish={() => this.initApp()}
+        onError={console.warn}
+      />);
     }
 
     return (
